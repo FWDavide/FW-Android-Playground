20e852db469ffbf8e096d70b9799b6d5b59c238d
dimitrios <dimitrios@futureworkshops.com>
2017-08-30 10:54:28 +0100

    Updated login Module and deleted Login Component. It is not required with dagger 2.11, unless we need the module to have a constructor. Otherwise Dagger takes care of generating a component.

--- fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/login/dagger/LoginModule.java
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/login/dagger/LoginModule.java
@@ -1,29 +1,34 @@
 package com.futureworkshops.android.architecture.presentation.login.dagger;
 
 
+import android.app.Activity;
+
+import com.futureworkshops.android.architecture.domain.dagger.module.ApplicationModule;
+import com.futureworkshops.android.architecture.domain.dagger.module.NetModule;
+import com.futureworkshops.android.architecture.domain.dagger.scope.ActivityScope;
 import com.futureworkshops.android.architecture.domain.network.RestManager;
+import com.futureworkshops.android.architecture.presentation.login.LoginActivity;
 import com.futureworkshops.android.architecture.presentation.login.LoginContract;
 import com.futureworkshops.android.architecture.presentation.login.LoginInteractor;
 
+import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 
+@ActivityScope
 @Module
-public class LoginModule {
+public abstract class LoginModule {
 
-    private LoginContract.View loginView;
+    @Binds
+    abstract Activity bindActivity(LoginActivity activity);
 
-    public LoginModule(LoginContract.View loginView) {
-        this.loginView = loginView;
-    }
+    //This method binds the LoginActivity to LoginContract.View. When the LoginContract.View is requested, the activity is returned.
+    @Binds
+    abstract LoginContract.View bindMvpView(LoginActivity activity);
 
     @Provides
-    LoginInteractor providesLoginInteractor(RestManager restManager) {
+    static LoginInteractor providesLoginInteractor(RestManager restManager) {
         return new LoginInteractor(restManager);
     }
 
-    @Provides
-    LoginContract.View providesLoginView() {
-        return loginView;
-    }
 }
