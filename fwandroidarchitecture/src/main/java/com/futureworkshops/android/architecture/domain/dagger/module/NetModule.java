5f67c675f0ab23dff81e14947cdf7d7ea21a163c
dimitrios <dimitrios@futureworkshops.com>
2017-09-04 16:12:11 +0100

    feature: removed unused imports and NetworkUtil (currently unused).

--- fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/domain/dagger/module/NetModule.java
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/domain/dagger/module/NetModule.java
@@ -6,10 +6,7 @@ import com.futureworkshops.android.architecture.BuildConfig;
 import com.futureworkshops.android.architecture.domain.network.RestApi;
 import com.futureworkshops.android.architecture.domain.network.RestManager;
 import com.futureworkshops.android.architecture.domain.rx.scheduler.WorkerSchedulerProvider;
-import com.futureworkshops.android.architecture.presentation.util.NetworkUtil;
 import com.google.gson.Gson;
-
-import java.io.IOException;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -17,10 +14,7 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
