8038f508e8541e1f4549aef25d77f822fb6c84c6
dimitrios <dimitrios@futureworkshops.com>
2017-09-05 16:14:54 +0100

    feature: re-configured the OkHttpClient interceptors for caching and re-added NetworkUtil. It's required by the Interceptors.

--- fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/domain/dagger/module/NetModule.java
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/domain/dagger/module/NetModule.java
@@ -6,7 +6,11 @@ import com.futureworkshops.android.architecture.BuildConfig;
 import com.futureworkshops.android.architecture.domain.network.RestApi;
 import com.futureworkshops.android.architecture.domain.network.RestManager;
 import com.futureworkshops.android.architecture.domain.rx.scheduler.WorkerSchedulerProvider;
+import com.futureworkshops.android.architecture.presentation.util.NetworkUtil;
 import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -14,7 +18,11 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -33,9 +41,13 @@ public class NetModule {
 
     private final static String NAME_RETROFIT_DEFAULT = "NAME_RETROFIT_DEFAULT";
 
+    private static final String HEADER_CACHE_CONTROL = "Cache-Control";
+
     private String mBaseUrl = "http://www.baseUrl.com";
 
     private static final int CACHE_SIZE = 50 * 1024 * 1024; //50 MB
+    private static final int CACHE_MAX_AGE = 60 * 60 * 24; //24 Hours
+
 
     @Singleton
     @Named(NAME_RETROFIT_DEFAULT)
@@ -49,8 +61,40 @@ public class NetModule {
             httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.NONE);
         }
 
+        final Interceptor cacheNetworkInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+
+                Response response = chain.proceed(chain.request());
+
+                CacheControl cacheControl = new CacheControl.Builder()
+                        .maxAge(1, TimeUnit.DAYS)
+                        .build();
+                return response.newBuilder()
+                        .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+                        .build();
+            }
+        };
+
+        Interceptor offlineCacheInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                if (NetworkUtil.isNetworkAvailable(context)) {
+                    CacheControl cacheControl = new CacheControl.Builder()
+                            .maxStale(1, TimeUnit.DAYS)
+                            .build();
+                    request = request.newBuilder().header(HEADER_CACHE_CONTROL, cacheControl.toString()).build();
+                }
+                return chain.proceed(request);
+            }
+        };
+
+
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(httpLoggingInterceptor)
+                .addInterceptor(offlineCacheInterceptor)
+                .addNetworkInterceptor(cacheNetworkInterceptor)
                 .cache(new Cache(context.getCacheDir(), CACHE_SIZE))
                 .build();
 
---
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/util/NetworkUtil.java
@@ -0,0 +1,20 @@
+package com.futureworkshops.android.architecture.presentation.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by dimitrios on 05/09/2017.
+ */
+
+public class NetworkUtil {
+
+    public static boolean isNetworkAvailable(@NonNull Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager == null) return false;
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
