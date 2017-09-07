af0a9f079c31ded84b09f48c5173aa9cb118f66f
vlad <vlad@futureworkshops.com>
2017-09-07 14:45:57 +0100

    Creates mock for the rest api.

---
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/domain/rx/FakeRestApi.java
@@ -0,0 +1,21 @@
+package com.futureworkshops.android.architecture.domain.rx;
+
+import com.futureworkshops.android.architecture.domain.network.RestApi;
+import com.futureworkshops.android.architecture.model.User;
+
+import java.util.Map;
+
+import io.reactivex.Single;
+
+
+public class FakeRestApi implements RestApi {
+
+    @Override
+    public Single<User> login(String contentType, Map<String, String> fieldMap) {
+        return Single.just(user());
+    }
+
+    private User user() {
+        return new User(" name", "email", "address");
+    }
+}
