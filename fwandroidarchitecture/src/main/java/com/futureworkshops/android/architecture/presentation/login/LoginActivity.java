55c729caadef57e59bcc08699f18ba10ec0de7e7
vlad <vlad@futureworkshops.com>
2017-09-07 14:47:06 +0100

    Starts login when the user presses the button.

--- fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/login/LoginActivity.java
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/login/LoginActivity.java
@@ -1,21 +1,42 @@
 package com.futureworkshops.android.architecture.presentation.login;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.futureworkshops.android.architecture.R;
+import com.futureworkshops.android.architecture.domain.network.RestManager;
+import com.futureworkshops.android.architecture.domain.rx.FakeRestApi;
+import com.futureworkshops.android.architecture.domain.rx.scheduler.WorkerSchedulerProvider;
 import com.futureworkshops.android.architecture.presentation.common.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.futureworkshops.android.architecture.domain.network.RestManager.PASSWORD;
+import static com.futureworkshops.android.architecture.domain.network.RestManager.USERNAME;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends BaseActivity implements LoginContract.View {
 
+    LoginPresenter loginPresenter =
+            new LoginPresenter(
+                    new LoginInteractor(
+                            new RestManager(new FakeRestApi(), new WorkerSchedulerProvider())));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.email_sign_in_button)
+    public void onLoginClicked() {
+        loginPresenter.login(USERNAME, PASSWORD);
     }
 
     @Override
