b97e0cccf82b24e65ac7ff00ef6aacb6992109e1
stelian <stelian@futureworkshops.com>
2017-09-18 12:12:05 +0100

    chore:#, set the SchedulerProvider

--- fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/FwAndroidArchitetureApp.java
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/FwAndroidArchitetureApp.java
@@ -3,13 +3,14 @@ package com.futureworkshops.android.architecture.presentation;
 
 import com.futureworkshops.android.architecture.domain.dagger.AppComponent;
 import com.futureworkshops.android.architecture.domain.dagger.DaggerAppComponent;
+import com.futureworkshops.android.architecture.domain.rx.scheduler.WorkerSchedulerProvider;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
 /**
  * Created by stelian on 28/08/2017.
- *
+ * <p>
  * extending {@link DaggerApplication} is the equivilent of implementing {@link dagger.android.HasActivityInjector} and
  * injecting a {@link dagger.android.DispatchingAndroidInjector}. What this means, is our Application will provide
  * {@link AndroidInjector} to our Activities.
@@ -27,7 +28,10 @@ public class FwAndroidArchitetureApp extends DaggerApplication {
      */
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        AppComponent appComponent = DaggerAppComponent.builder().application(this).build();
+        AppComponent appComponent = DaggerAppComponent.builder()
+                .application(this)
+                .schedulerProvider(new WorkerSchedulerProvider())
+                .build();
         appComponent.inject(this);
         return appComponent;
     }
