7060286d3ff8b9abd2a9bebda5a7d0a05d4a68bc
stelian <stelian@futureworkshops.com>
2017-09-18 11:57:14 +0100

    chore:#, add view as a parameter to the presenter constructor

--- fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/login/LoginPresenter.java
+++ fwandroidarchitecture/src/main/java/com/futureworkshops/android/architecture/presentation/login/LoginPresenter.java
@@ -2,6 +2,8 @@ package com.futureworkshops.android.architecture.presentation.login;
 
 import android.support.annotation.NonNull;
 
+import java.lang.ref.WeakReference;
+
 import io.reactivex.observers.DisposableCompletableObserver;
 
 /**
@@ -11,9 +13,11 @@ import io.reactivex.observers.DisposableCompletableObserver;
 public class LoginPresenter implements LoginContract.Presenter {
 
     private final LoginInteractor loginInteractor;
+    private final WeakReference<LoginContract.View> mViewWeakReference;
 
-    public LoginPresenter(LoginInteractor loginInteractor) {
+    public LoginPresenter(LoginInteractor loginInteractor, LoginContract.View view) {
         this.loginInteractor = loginInteractor;
+        mViewWeakReference = new WeakReference<LoginContract.View>(view);
     }
 
     @Override
