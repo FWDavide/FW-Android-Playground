5d37f739ee10051b578412a942e8d38fd081bf94
stelian <stelian@futureworkshops.com>
2018-04-03 18:06:56 +0100

    chore:#, add Dagger components and modules

---
+++ marvelHeroes/src/main/java/com/futureworkshops/marvelheroes/domain/dagger/ActivityComponentBindModule.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 FutureWorkshops. All rights reserved.
+ */
+
+package com.futureworkshops.marvelheroes.domain.dagger;
+
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+/**
+ * Created by dimitrios on 24/08/2017.
+ *
+ * This Module's purpose, is to define which Activity depends on which Module.
+ * By providing this Module to our {@link AppComponent}
+ * we are allowing Dagger to generate SubComponents and inject our activities.
+ * The benefit of this approach, is we don't have to define Dagger Components for our Modules, with
+ * the exception of our top-level AppComponent.
+ * Subcomponents are components that live below the AppComponent in our graph.
+ */
+@Module
+public abstract class ActivityComponentBindModule {
+
+//    @ContributesAndroidInjector(modules = LoginModule.class)
+//    abstract LoginActivity bindLoginActivity();
+
+}
