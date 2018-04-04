4be16dd8c6f5bdb86007c2205c05d71570a03820
stelian <stelian@futureworkshops.com>
2018-04-04 17:49:12 +0100

    chore:#, fix status bar colour for SDK < 23

--- marvelHeroes/src/main/java/com/futureworkshops/marvelheroes/presentation/common/BaseActivity.java
+++ marvelHeroes/src/main/java/com/futureworkshops/marvelheroes/presentation/common/BaseActivity.java
@@ -4,11 +4,15 @@
 
 package com.futureworkshops.marvelheroes.presentation.common;
 
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.futureworkshops.marvelheroes.R;
 
@@ -19,11 +23,18 @@ import butterknife.BindView;
  */
 
 public class BaseActivity extends AppCompatActivity {
-
+    
     @Nullable
     @BindView(R.id.toolbar)
     protected Toolbar mToolbar;
-
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adjustThemeColours();
+        
+    }
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -34,7 +45,7 @@ public class BaseActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
+    
     protected void setupToolbar(boolean showHomeAsUp) {
         if (mToolbar != null) {
             setSupportActionBar(mToolbar);
@@ -42,5 +53,20 @@ public class BaseActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(false);
         }
     }
-
+    
+    /**
+     * Pre-Marshmallow devices don't support styling the status bar text and icon colours in case
+     * we have a really light background color for the status bar! To make status bar text and icons
+     * visible on these devices we manually change the status bar background.
+     */
+    private void adjustThemeColours() {
+        if (Build.VERSION.SDK_INT < VERSION_CODES.M) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            
+            window.setStatusBarColor(getResources().getColor(R.color.textColorSecondary));
+            
+        }
+    }
+    
 }
