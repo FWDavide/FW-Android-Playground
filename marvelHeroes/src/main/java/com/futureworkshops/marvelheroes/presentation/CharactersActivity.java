55c2cac510993088c9509b0445e8a2d89cc388d8
stelian <stelian@futureworkshops.com>
2018-04-04 12:30:45 +0100

    chore:#, add CharactersActivity stub

---
+++ marvelHeroes/src/main/java/com/futureworkshops/marvelheroes/presentation/CharactersActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018 FutureWorkshops. All rights reserved.
+ */
+
+package com.futureworkshops.marvelheroes.presentation;
+
+import android.os.Bundle;
+
+import com.futureworkshops.marvelheroes.R;
+import com.futureworkshops.marvelheroes.presentation.common.BaseActivity;
+
+public class CharactersActivity extends BaseActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_characters);
+    }
+}
